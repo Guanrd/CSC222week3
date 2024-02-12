95ddde6f7b9fba3e8b374ba1c3e194536aa4e907
Richard Guan <richardguan00@gmail.com>
2024-02-12 16:02:01 -0500

    Implemented user input for their name

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+    Ask a user for their first name
+    Print it out
+ */
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     System.out.println("We are at week 3");
-    }
-}
+
+    //Create a variable that will hold the user's name
+    String name;
+    //Create a new Scanner object, so that I can get input from the user
+    Scanner input = new Scanner(System.in);
+    // ask the user what's their first name using a print statement
+    System.out.print("What's your first name? ");
+    name = input.nextLine();
+
+    //Print out the user's input using the name variable
+    System.out.println(name);
+
+    //Print out the name using string literals/concatenation
+    System.out.println("You entered:"+name);
+    }//ends main method
+
+}//ends Main class
