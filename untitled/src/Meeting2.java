c229b5cfb5d843e6f07a4a12dee0e66e89e7bda0
Richard Guan <richardguan00@gmail.com>
2024-02-12 16:23:50 -0500

    Add a new file for meeting2 in week3

---
+++ untitled/src/Meeting2.java
@@ -0,0 +1,23 @@
+/*
+    File used in week3 meeting2 to learn new code
+ */
+public class Meeting2 {
+    public static void main(String[] args) {
+        System.out.println("Meeting 2 Week 3");
+        //Specify the pet's name in String variable
+        String petName = "Rover";
+        //Specify the pet's age in int variable
+        int petAge = 10;
+        //Print what's in the variables
+        System.out.println("Pet name: " + petName);
+        System.out.println("Pet age: " + petAge);
+
+        //Compute the dog's human years to dog years human years x 7
+        //petAge = petAge * 7 ;
+        //Create new int variable to hold the pet age conversion
+        int petAgeInDogYears = petAge * 7;
+        System.out.println("Pet age in dog years: " + petAgeInDogYears);
+
+
+    }//ends main method/driver
+}//ends Meeting2 class
